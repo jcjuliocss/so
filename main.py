84fb479a6be911f5293fdc9ceb7551d04281ff6e
Julio Cesar <39426877+jcjuliocss@users.noreply.github.com>
2020-11-19 20:26:58 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import threading
+import subprocess
 
 class GerenciadorTarefas(threading.Thread):
 	def __init__(self):
@@ -9,6 +10,7 @@ class GerenciadorTarefas(threading.Thread):
 
 	def run(self):
 		while True:
+			print('---------- GERENCIADOR DE TAREFAS ----------')
 			print('[ 1 ] Fazer login\n[ 2 ] Cadastrar usuario\n[ 3 ] Sair')
 			inp = input()
 			if int(inp) == 1:
@@ -17,16 +19,44 @@ class GerenciadorTarefas(threading.Thread):
 
 				self.lock.acquire()
 				try:
-					f = open('usuarios.txt', 'r')
-					usuarios = f.readlines()
-					f.close()
-					if [nome_login, senha_login] in [usuario.replace('\n', '').split('-') for usuario in usuarios]:
+					login = subprocess.run('./fazer_login.sh ' + str(nome_login) + ' ' + str(senha_login), text=True, capture_output=True, shell=True)
+					if int(login.returncode) == 1:
 						print("Login valido!")
 					else:
 						print("Login invalido!")
 						continue
 				finally:
 					self.lock.release()
+
+				finaliza_sessao = False
+				while not finaliza_sessao:
+					print('---------- Ola, {}! ----------'.format(nome_login))
+					print('[ 1 ] Adicionar tarefa\n[ 2 ] Listar tarefas\n[ 3 ] Sair')
+					inp = input()
+					if int(inp) == 1:
+						tarefa = input('Insira a tarefa: ')
+
+						self.lock.acquire()
+						try:
+							f = open('tarefas.txt', 'a')
+							f.write(str(tarefa) + '\n')
+							f.close()
+							print('Tarefa cadastrada com sucesso!')
+							continue
+						finally:
+							self.lock.release()
+							pass
+					elif int(inp) == 2:
+						self.lock.acquire()
+						try:
+							tarefas = subprocess.run('./buscar_tarefas.sh ', text=True, capture_output=True, shell=True)
+							print(tarefas.stdout)
+						finally:
+							self.lock.release()
+							pass
+					else:
+						finaliza_sessao = True
+
 			elif int(inp) == 2:
 				login_valido = False
 				while not login_valido:
@@ -51,7 +81,6 @@ class GerenciadorTarefas(threading.Thread):
 			else:
 				os.system('kill -9 ' + str(self.pid))
 				break
-
 
 gerenciador = GerenciadorTarefas()
 gerenciador.start()
